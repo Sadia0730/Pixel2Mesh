6b61a2f2135ecc3860bf2c0401bd8b8bf2803244
Sadia0730 <31884897+Sadia0730@users.noreply.github.com>
2023-09-26 14:50:48 -0500

    Update demo.py

--- Pixel2Mesh-master/demo.py
+++ Pixel2Mesh-master/demo.py
@@ -84,4 +84,4 @@ mesh = np.vstack((vert, face))
 pred_path = FLAGS.image.replace('.png', '.obj')
 np.savetxt(pred_path, mesh, fmt='%s', delimiter=' ')
 
-print 'Saved to', pred_path
+print ('Saved to', pred_path)
